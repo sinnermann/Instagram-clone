44e3ca23393d3b7076e514c81c10771e08b92d2e
sinnerman <sinnerman@gmail.com>
2022-12-09 18:20:27 +0400

    creating searchbar on the ehader

--- components/Header.js
+++ components/Header.js
@@ -1,29 +1,38 @@
 import React from "react";
 import Image from "next/image";
+import { SearchIcon } from "@heroicons/react/outline";
 
 const Header = () => {
   return (
-    <div>
+    <div className="flex items-center justify-between max-w-6xl">
       {/* Left */}
-      <div className="flex items-center justify-between max-w-6xl">
-        <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
-          <Image
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png"
-            layout="fill"
-            className="object-contain"
-          />
-        </div>
-        <div className="cursor-pointer h-24 w-10 relative lg:hidden">
-          <Image
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/1200px-Instagram_logo_2022.svg.png"
-            layout="fill"
-            className="object-contain"
-          />
-        </div>
-        <h1>Right side</h1>
+      <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png"
+          layout="fill"
+          className="object-contain"
+        />
+      </div>
+      <div className="cursor-pointer h-24 w-10 relative lg:hidden">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/1200px-Instagram_logo_2022.svg.png"
+          layout="fill"
+          className="object-contain"
+        />
       </div>
       {/* Middle */}
+      <div className="relative mt-1">
+        <div className="absolute top-2 left-2">
+          <SearchIcon className="h-5 text-gray-500" />
+        </div>
+        <input
+          type="text"
+          placeholder="Search"
+          className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
+        />
+      </div>
       {/* right */}
+      <h1>Right side</h1>
     </div>
   );
 };
