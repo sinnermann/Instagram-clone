eedd9a7927541fed59d62424100019da9f6729fc
sinnerman <sinnerman@gmail.com>
2022-12-09 19:27:07 +0400

    making header sticky

--- components/Header.js
+++ components/Header.js
@@ -5,42 +5,44 @@ import { HomeIcon } from "@heroicons/react/solid";
 
 const Header = () => {
   return (
-    <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
-      {/* Left */}
-      <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
-        <Image
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png"
-          layout="fill"
-          className="object-contain"
-        />
-      </div>
-      <div className="cursor-pointer h-24 w-10 relative lg:hidden">
-        <Image
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/1200px-Instagram_logo_2022.svg.png"
-          layout="fill"
-          className="object-contain"
-        />
-      </div>
-      {/* Middle */}
-      <div className="relative mt-1">
-        <div className="absolute top-2 left-2">
-          <SearchIcon className="h-5 text-gray-500" />
+    <div className="shadow-sm border-b sticky top-0 bg-white z-30">
+      <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
+        {/* Left */}
+        <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
+          <Image
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png"
+            layout="fill"
+            className="object-contain"
+          />
         </div>
-        <input
-          type="text"
-          placeholder="Search"
-          className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
-        />
-      </div>
-      {/* right */}
-      <div className="flex space-x-4 items-center cursor-pointer">
-        <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
-        <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
-        <img
-          src="https://media1.popsugar-assets.com/files/thumbor/ZCWD9YXxqYzk9riO2WR2OrxzWUw/721x0:1801x1080/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2019/07/01/098/n/46207611/5d2cc4f65d1ab1d1992803.52716266_/i/Why-Tony-Stark-Best-Marvel-Character.jpg"
-          alt="user"
-          className="h-10 w-10 rounded-full"
-        />
+        <div className="cursor-pointer h-24 w-10 relative lg:hidden">
+          <Image
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/1200px-Instagram_logo_2022.svg.png"
+            layout="fill"
+            className="object-contain"
+          />
+        </div>
+        {/* Middle */}
+        <div className="relative mt-1">
+          <div className="absolute top-2 left-2">
+            <SearchIcon className="h-5 text-gray-500" />
+          </div>
+          <input
+            type="text"
+            placeholder="Search"
+            className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
+          />
+        </div>
+        {/* right */}
+        <div className="flex space-x-4 items-center cursor-pointer">
+          <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+          <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+          <img
+            src="https://media1.popsugar-assets.com/files/thumbor/ZCWD9YXxqYzk9riO2WR2OrxzWUw/721x0:1801x1080/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2019/07/01/098/n/46207611/5d2cc4f65d1ab1d1992803.52716266_/i/Why-Tony-Stark-Best-Marvel-Character.jpg"
+            alt="user"
+            className="h-10 w-10 rounded-full"
+          />
+        </div>
       </div>
     </div>
   );
