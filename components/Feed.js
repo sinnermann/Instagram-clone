9b6304a0f48e13f355cea7d392881d5d5f542de0
sinnerman <sinnerman@gmail.com>
2022-12-09 20:19:30 +0400

    starting the feed section, populating data usefing fake data using minifaker & map through it

---
+++ components/Feed.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Stories from "./Stories";
+
+const Feed = () => {
+  return (
+    <main>
+      <section>
+        {/* Stories*/}
+        <Stories />
+        {/* Stories*/}
+      </section>
+      <section>
+        {/* Mini profile*/}
+        {/* Suggestions*/}
+      </section>
+    </main>
+  );
+};
+
+export default Feed;
