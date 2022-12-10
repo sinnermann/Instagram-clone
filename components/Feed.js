ca3a426421c1cc4d98e860f5e8399ba0d4b7357e
sinnerman <sinnerman@gmail.com>
2022-12-10 19:10:26 +0400

    putting styles for posts

--- components/Feed.js
+++ components/Feed.js
@@ -4,14 +4,14 @@ import Stories from "./Stories";
 
 const Feed = () => {
   return (
-    <main>
-      <section>
+    <main className="grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto">
+      <section className="md:col-span-2">
         {/* Stories*/}
         <Stories />
         {/* Posts*/}
         <Posts />
       </section>
-      <section>
+      <section className="hidden md:inline-grid md:col-span-1">
         {/* Mini profile*/}
         {/* Suggestions*/}
       </section>
