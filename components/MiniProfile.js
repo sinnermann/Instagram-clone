ee3e34b400e1d1f22663cf55520a433b10d982df
sinnerman <sinnerman@gmail.com>
2022-12-13 20:04:15 +0400

    Modification of mini profile & stories & story components to include sessions & made all responsive

--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { useSession, signOut } from "next-auth/react";
 
 const MiniProfile = () => {
+  const { data: session } = useSession();
   return (
     <div className="flex items-center justify-between mt-14 ml-10">
       <img
@@ -9,10 +11,12 @@ const MiniProfile = () => {
         alt="username"
       />
       <div className="flex-1 ml-4">
-        <h2 className="font-fold">Ironman</h2>
+        <h2 className="font-fold">{session?.user.username}</h2>
         <h3 className="text-sm text-gray-400">Welcome to Instagram</h3>
       </div>
-      <button className="font-semibold text-blue-400 text-sm">Sigh out</button>
+      <button onClick={signOut} className="font-semibold text-blue-400 text-sm">
+        Sigh out
+      </button>
     </div>
   );
 };
--- components/Stories.js
+++ components/Stories.js
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from "react";
 import minifaker from "minifaker";
 import "minifaker/locales/en";
 import Story from "./Story";
+import { useSession } from "next-auth/react";
 
 const Stories = () => {
   const [storyUsers, setStoryUsers] = useState([]);
+  const { data: session } = useSession();
   useEffect(() => {
     const storyUsers = minifaker.array(20, (i) => ({
       username: minifaker.username({ locale: "en" }).toLowerCase(),
@@ -16,6 +18,13 @@ const Stories = () => {
   }, []);
   return (
     <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 border overflow-x-scroll rounded-sm scrollbar-none">
+      {session && (
+        <Story
+          img="https://media1.popsugar-assets.com/files/thumbor/ZCWD9YXxqYzk9riO2WR2OrxzWUw/721x0:1801x1080/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2019/07/01/098/n/46207611/5d2cc4f65d1ab1d1992803.52716266_/i/Why-Tony-Stark-Best-Marvel-Character.jpg" //or put this {//session.user.image} to display google image
+          username={session.user.username}
+          isUser="true"
+        />
+      )}
       {storyUsers.map((user) => (
         <Story key={user.id} username={user.username} img={user.img} />
       ))}
