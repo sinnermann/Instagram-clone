55821cf60a2674c82378692190a8223493b3432a
sinnerman <sinnerman@gmail.com>
2022-12-14 17:57:38 +0400

    installing & implementing react-modal

--- components/Posts.js
+++ components/Posts.js
@@ -8,7 +8,7 @@ const Posts = () => {
       username: "Ironman",
       userImage:
         "https://media1.popsugar-assets.com/files/thumbor/ZCWD9YXxqYzk9riO2WR2OrxzWUw/721x0:1801x1080/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2019/07/01/098/n/46207611/5d2cc4f65d1ab1d1992803.52716266_/i/Why-Tony-Stark-Best-Marvel-Character.jpg",
-      img: "https://crops.giga.de/78/94/09/01d85c44b7f4a73043d9ab7f0c_YyAzMzg0eDE5MDQrMTA4KzI0OAJyZSA0ODAgMjcwAzc0MDg0N2EwZGIw.jpg",
+      img: "https://imgix.bustle.com/uploads/image/2018/7/31/1f186f0a-224e-430c-90b3-87ca11f21f73-kyles.jpg?w=1200&h=630&fit=crop&crop=faces&fm=jpg",
       caption: "Looking cool",
     },
     {
@@ -16,8 +16,16 @@ const Posts = () => {
       username: "BlackWidow",
       userImage:
         "https://assets-prd.ignimgs.com/2021/03/23/black-widow-button-1616528351974.jpg",
-      img: "https://www.tvinsider.com/wp-content/uploads/2022/09/cobra-kai-season-5-1014x570.jpg",
-      caption: "Miagi-do Rocks",
+      img: "https://metro.co.uk/wp-content/uploads/2019/10/PRC_91771418.jpg?quality=90&strip=all",
+      caption: "Looking Cool",
+    },
+    {
+      id: "3",
+      username: "BlackWidow",
+      userImage:
+        "https://assets-prd.ignimgs.com/2021/03/23/black-widow-button-1616528351974.jpg",
+      img: "https://images.ladbible.com/resize?type=jpeg&quality=70&width=720&fit=contain&gravity=null&dpr=1&url=https://eu-images.contentstack.com/v3/assets/blt8bbf16c2d7a209e5/blt7a98e5961c2760a7/62e0dd926d0e9331b618ca99/Instagram.png",
+      caption: "Looking Cool",
     },
   ];
   return (
--- components/UploadModal.js
+++ components/UploadModal.js
@@ -1,13 +1,23 @@
 import React from "react";
 import { modalState } from "../atom/modalAtom";
 import { useRecoilState } from "recoil";
+import Modal from "react-modal";
 
 const UploadModal = () => {
   const [open, setOpen] = useRecoilState(modalState);
   return (
     <div>
-      <h1>UploadModal</h1>
-      {open && <h1>The Modal is open</h1>}
+      {open && (
+        <Modal
+          className="max-w-lg w-[90%] h-[300px] absolute top-56 left-[50%] translate-x-[-50%] bg-white border-2 rounded-md shadow-md"
+          isOpen={open}
+          onRequestClose={() => setOpen(false)}
+        >
+          <div className="flex flex-col justify-center items-center h-[100%]">
+            <h1>Modal</h1>
+          </div>
+        </Modal>
+      )}
     </div>
   );
 };
