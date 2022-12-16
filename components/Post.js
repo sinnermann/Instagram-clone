5589dfa5b38375fcd653c7ec0e8fd792bfd03153
sinnerman <sinnerman@gmail.com>
2022-12-16 20:16:22 +0400

    Adding likes numbers

--- components/Post.js
+++ components/Post.js
@@ -108,6 +108,9 @@ const Post = ({ img, userImg, username, caption, id }) => {
 
       {/* Post comments  */}
       <p className="p-5 truncate">
+        {likes.length > 0 && (
+          <p className="font-bold mb-1">{likes.length} likes</p>
+        )}
         <span className="font-bold mr-2">{username}</span>
         {caption}
       </p>
